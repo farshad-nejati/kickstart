55d3d79c1193e20165fc800c2fa84abe91abac0c
farshad-nejati <nejati.farshad93@gmail.com>
2019-05-12 02:28:19 +0430

    add navigation to header

--- .next/dist/components/Header.js
+++ .next/dist/components/Header.js
@@ -10,6 +10,8 @@ var _react2 = _interopRequireDefault(_react);
 
 var _semanticUiReact = require('semantic-ui-react');
 
+var _routes = require('../routes');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var _jsxFileName = 'F:\\Project\\Blockchain\\Apps\\Kickstart\\components\\Header.js';
@@ -17,27 +19,36 @@ var _jsxFileName = 'F:\\Project\\Blockchain\\Apps\\Kickstart\\components\\Header
 exports.default = function () {
     return _react2.default.createElement(_semanticUiReact.Menu, { style: { marginTop: '10px' }, __source: {
             fileName: _jsxFileName,
-            lineNumber: 6
+            lineNumber: 8
         }
-    }, _react2.default.createElement(_semanticUiReact.Menu.Item, {
-        __source: {
+    }, _react2.default.createElement(_routes.Link, { route: '/', __source: {
             fileName: _jsxFileName,
-            lineNumber: 7
+            lineNumber: 9
         }
-    }, 'CrowdCoint'), _react2.default.createElement(_semanticUiReact.Menu.Menu, { position: 'right', __source: {
+    }, _react2.default.createElement('a', { className: 'item', __source: {
             fileName: _jsxFileName,
             lineNumber: 10
         }
-    }, _react2.default.createElement(_semanticUiReact.Menu.Item, {
-        __source: {
+    }, 'CrowdCoin')), _react2.default.createElement(_semanticUiReact.Menu.Menu, { position: 'right', __source: {
             fileName: _jsxFileName,
-            lineNumber: 11
+            lineNumber: 12
         }
-    }, 'Campaigns'), _react2.default.createElement(_semanticUiReact.Menu.Item, {
-        __source: {
+    }, _react2.default.createElement(_routes.Link, { route: '/', __source: {
+            fileName: _jsxFileName,
+            lineNumber: 13
+        }
+    }, _react2.default.createElement('a', { className: 'item', __source: {
             fileName: _jsxFileName,
             lineNumber: 14
         }
-    }, '+')));
+    }, 'Campaigns')), _react2.default.createElement(_routes.Link, { route: '/campaigns', __source: {
+            fileName: _jsxFileName,
+            lineNumber: 16
+        }
+    }, _react2.default.createElement('a', { className: 'item', __source: {
+            fileName: _jsxFileName,
+            lineNumber: 17
+        }
+    }, '+'))));
 };
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudHNcXEhlYWRlci5qcyJdLCJuYW1lcyI6WyJSZWFjdCIsIk1lbnUiLCJtYXJnaW5Ub3AiXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFBLEFBQU87Ozs7QUFDUCxBQUFRLEFBRVI7Ozs7OztrQkFBZSxZQUFNLEFBQ2pCOzJCQUNJLEFBQUMsdUNBQUssT0FBTyxFQUFFLFdBQWYsQUFBYSxBQUFhO3NCQUExQjt3QkFBQSxBQUNJO0FBREo7S0FBQSxrQkFDSyxjQUFELHNCQUFBLEFBQU07O3NCQUFOO3dCQUFBO0FBQUE7QUFBQSxPQURKLEFBQ0ksQUFHQSwrQkFBQyxjQUFELHNCQUFBLEFBQU0sUUFBSyxVQUFYLEFBQW9CO3NCQUFwQjt3QkFBQSxBQUNJO0FBREo7dUJBQ0ssY0FBRCxzQkFBQSxBQUFNOztzQkFBTjt3QkFBQTtBQUFBO0FBQUEsT0FESixBQUNJLEFBR0EsOEJBQUMsY0FBRCxzQkFBQSxBQUFNOztzQkFBTjt3QkFBQTtBQUFBO0FBQUEsT0FUWixBQUNJLEFBSUksQUFJSSxBQU1mO0FBaEJEIiwiZmlsZSI6IkhlYWRlci5qcyIsInNvdXJjZVJvb3QiOiJGOi9Qcm9qZWN0L0Jsb2NrY2hhaW4vQXBwcy9LaWNrc3RhcnQifQ==
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudHNcXEhlYWRlci5qcyJdLCJuYW1lcyI6WyJSZWFjdCIsIk1lbnUiLCJMaW5rIiwibWFyZ2luVG9wIl0sIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQSxBQUFPOzs7O0FBQ1AsQUFBUTs7QUFDUixBQUFTLEFBQVksQUFHckI7Ozs7OztrQkFBZSxZQUFNLEFBQ2pCOzJCQUNJLEFBQUMsdUNBQUssT0FBTyxFQUFFLFdBQWYsQUFBYSxBQUFhO3NCQUExQjt3QkFBQSxBQUNJO0FBREo7S0FBQSxrQkFDSSxBQUFDLDhCQUFLLE9BQU4sQUFBWTtzQkFBWjt3QkFBQSxBQUNJO0FBREo7dUJBQ0ksY0FBQSxPQUFHLFdBQUgsQUFBYTtzQkFBYjt3QkFBQTtBQUFBO09BRlIsQUFDSSxBQUNJLEFBRUosK0JBQUMsY0FBRCxzQkFBQSxBQUFNLFFBQUssVUFBWCxBQUFvQjtzQkFBcEI7d0JBQUEsQUFDSTtBQURKO3VCQUNJLEFBQUMsOEJBQUssT0FBTixBQUFZO3NCQUFaO3dCQUFBLEFBQ0k7QUFESjt1QkFDSSxjQUFBLE9BQUcsV0FBSCxBQUFhO3NCQUFiO3dCQUFBO0FBQUE7T0FGUixBQUNJLEFBQ0ksQUFFSiwrQkFBQSxBQUFDLDhCQUFLLE9BQU4sQUFBWTtzQkFBWjt3QkFBQSxBQUNJO0FBREo7dUJBQ0ksY0FBQSxPQUFHLFdBQUgsQUFBYTtzQkFBYjt3QkFBQTtBQUFBO09BVmhCLEFBQ0ksQUFJSSxBQUlJLEFBQ0ksQUFLbkI7QUFoQkQiLCJmaWxlIjoiSGVhZGVyLmpzIiwic291cmNlUm9vdCI6IkY6L1Byb2plY3QvQmxvY2tjaGFpbi9BcHBzL0tpY2tzdGFydCJ9
